e202553b14b0923aa56aecc9691208aa67445d0d
Zack Brown <zack@iamexec.com>
2013-08-10 19:30:24 -0700

    add car icon, place on map

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,16 +1,25 @@
 'use strict';
 
-RideshareSimApp.controller('MainCtrl', function($scope) {
+function onGoogleReady() {
+  alert('ready!');
+}
+
+RideshareSimApp.controller('MainCtrl', function($scope, $timeout) {
 
   var mapOptions = {
           center: new google.maps.LatLng(37.76, -122.435),
           zoom: 13,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-  $scope.map = new google.maps.Map(document.getElementById("google-maps-canvas"),
-      mapOptions);
+  $scope.map = new google.maps.Map(document.getElementById("google-maps-canvas"), mapOptions);
+  var image = {
+    url: 'img/yellow-car-top.png'
+  }
+  var myLatLng = new google.maps.LatLng(37.76, -122.435);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: $scope.map,
+    icon: image
+  })
 
-  google.maps.event.addListenerOnce($scope.map, 'idle', function() {
-    google.maps.event.trigger($scope.map, 'resize');
-  });
 });
